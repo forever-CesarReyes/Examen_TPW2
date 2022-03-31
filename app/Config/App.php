2cea342223f514b002765c27bbf547cc9e99f0b9
forever-CesarReyes <rr.cesargabriel@gmail.com>
2022-03-31 00:12:50 -0600

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/Examen_TPW2';
+    public $baseURL = 'http://localhost/';
 
     /**
      * --------------------------------------------------------------------------
