86686bad08fae40f3449fcf1a1cc7db3295f71a1
forever-CesarReyes <rr.cesargabriel@gmail.com>
2022-03-30 23:31:28 -0600

    combo

--- app/Views/vRegistro_gasto.php
+++ app/Views/vRegistro_gasto.php
@@ -8,6 +8,9 @@
     <title>Registro</title>
 </head>
 <body>
+    <?php
+    include_once ('funciones_comunes.php');
+    $opciones_combo_categorias=combo_categorias();?>
     <div class="container">
     <h1>Encuesta a clientes</h1>
     <form method="POST" action="../Home/insertarForm_gasto">
@@ -18,8 +21,9 @@
         <input type="date" id="fecha" name="fecha"><br>
         <label for="descripcion">descripcion</label>
         <input type="text" id="descripcion" name="descripcion"><br>
-        <label for="categoria">Categoria</label>
-        <input type="id_categoria" id="id_categoria" name="id_categoria"><br>
+        <select type="text"name ="id_categoria"id="id_categoria"required>" >
+         <?php echo $opciones_combo_categorias; ?>
+        </select>
 
 
         <button type="submit"> Registrar </button>
@@ -27,7 +31,7 @@
 </div><br>
 <footer>
 <div class="text-center text-dark p-3" style="background-color: #97A3AB ">
- Osvaldo
+ Osvaldo y Cesar
 </div>
 
 </footer>
